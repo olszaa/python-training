093ea7d82487bdc3f4d43af7c41d1d0eccdc378f
olszaa <49523202+olszaa@users.noreply.github.com>
2019-06-07 18:45:01 +0700

    Add files via upload

---
+++ compare_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+print("Welcome Siwadon")
+
+def compare(_num1, _num2, _num3):
+    _max = ""
+    _min = ""
+    if _num1 > _num2 and _num1 > _num3 :
+        _max = "num1"
+    elif _num2 > _num3 :
+        _max = "num2"
+    else:
+        _max = "num3"
+    if _num1 < _num2 and _num1 < _num3 :
+        _min = "num1"
+    elif _num2 < _num3 :
+        _min = "num2"
+    else:
+        _min = "num3"
+    
+    return (_max ,_min)
+
+num1 = int(input("Enter num1 :"))
+num2 = int(input("Enter num2 :"))
+num3 = int(input("Enter num3 :"))
+mylist = compare(num1,num2,num3)
+
+print("{0} as the greatest number \n{1} as the less number".format(mylist[0],mylist[1]))
+
+
+
+
